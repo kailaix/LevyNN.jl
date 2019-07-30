3e74a5ebaa34d435e448a519a86f595f83f86b7c
Kailai Xu <kailaix@stanford.edu>
2019-07-30 00:42:26 -0700

    fix run.sh

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,14 +1,14 @@
-julia levy-1.jl TruncatedNormal2D NN 2 1000  &
-julia levy-1.jl TruncatedNormal2D NN 5 1000  &
+julia levy-1.jl TruncatedNormal2D RBF 10 1000 &
 julia levy-1.jl TruncatedNormal2D NN 10 1000 &
+julia levy-1.jl TruncatedNormal2D PL 10 1000 &
 wait %1 %2 %3
 
-julia levy-1.jl TruncatedNormal2D RBF 10 1000 &
+julia levy-1.jl TruncatedNormal2D NN 2 1000  &
 julia levy-1.jl TruncatedNormal2D RBF 20 1000 &
 julia levy-1.jl TruncatedNormal2D RBF 40 1000 &
 wait %1 %2 %3
 
-julia levy-1.jl TruncatedNormal2D PL 10 1000 &
+julia levy-1.jl TruncatedNormal2D NN 5 1000  &
 julia levy-1.jl TruncatedNormal2D PL 20 1000 &
 julia levy-1.jl TruncatedNormal2D PL 40 1000 &
 wait %1 %2 %3
